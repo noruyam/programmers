4d69e223fefc504cad5cdae6f6cb2e7dae589fa8
noru <qkr2633911@naver.com>
2022-11-04 11:49:09 +0900

    Lv.0

---
+++ src/Lv0/Solution39.java
@@ -0,0 +1,19 @@
+package Lv0;
+
+import java.util.Stack;
+
+public class Solution39 {
+    /**
+     * 같은 숫자는 싫어
+     * @param arr
+     * @return
+     */
+    public int[] solution(int []arr) {
+        int[] answer = {};
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
