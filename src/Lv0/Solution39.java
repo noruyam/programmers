93b0343daf74cee2d584161278f1dda870aec1ad
noru <qkr2633911@naver.com>
2022-11-06 20:11:41 +0900

    Lv.0

--- src/Lv0/Solution39.java
+++ src/Lv0/Solution39.java
@@ -11,9 +11,6 @@ public class Solution39 {
     public int[] solution(int []arr) {
         int[] answer = {};
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println(" ");
-
         return answer;
     }
 }
