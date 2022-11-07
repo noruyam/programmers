964978541c12999a8cde09ada8d6847a27a684ce
KCC <qkr2633911@naver.com>
2022-11-07 09:44:07 +0900

    Lv.0

--- src/Lv0/Solution38.java
+++ src/Lv0/Solution38.java
@@ -3,6 +3,7 @@ package Lv0;
 public class Solution38 {
     /**
      * 분수의 덧셈
+     *
      * @param denum1
      * @param num1
      * @param denum2
@@ -10,7 +11,24 @@ public class Solution38 {
      * @return
      */
     public int[] solution(int denum1, int num1, int denum2, int num2) {
-        int[] answer = {};
+        int[] answer = new int[2];
+        int test = 0;
+        if (num1 > num2) {
+            test = num1 % num2;
+        } else if (num1 < num2) {
+            test = num2 % num1;
+        } else if (num1 == num2) {
+
+        }
+
+        if (test == 0) {
+            if (num1 < num2) {
+                answer[0] = denum1 * (num2 / num1);
+                answer[1] = num2;
+            } else {
+
+            }
+        }
         return answer;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
 import Lv0.Solution24;
 import Lv0.Solution28;
 import Lv0.Solution30;
+import Lv0.Solution39;
 
 public class Main {
     public static void main(String[] args) {
 
-        Solution30 solution28 = new Solution30();
+        Solution39 solution28 = new Solution39();
 
-        System.out.println( solution28.solution("1234","1"));
+        System.out.println( solution28.solution(new int[]{1, 2, 3, 4,4}));
     }
 
 }
