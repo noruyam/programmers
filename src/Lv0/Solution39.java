366cd370d10c3cc5c6754e79d430029f7645c1af
KCC <qkr2633911@naver.com>
2022-11-07 09:50:12 +0900

    Lv.0

--- src/Lv0/Solution39.java
+++ src/Lv0/Solution39.java
@@ -1,16 +1,38 @@
 package Lv0;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Solution39 {
     /**
      * 같은 숫자는 싫어
+     *
      * @param arr
      * @return
      */
-    public int[] solution(int []arr) {
-        int[] answer = {};
+    public int[] solution(int[] arr) {
 
+        Stack<Integer> stack = new Stack<>();
+        Queue<Integer> queue = new LinkedList<>();
+
+        stack.push(arr[0]);
+        queue.add(arr[0]);
+        for (int i = 0; i < arr.length; i++) {
+            if (stack.search(arr[i]) > 0) {
+            } else {
+                stack.push(arr[i]);
+                System.out.println(stack.peek());
+            }
+        }
+        int idx = 0;
+        int[] answer = new int[stack.size()];
+        for (int a : stack) {
+            answer[idx]=a;
+            idx++;
+            System.out.println(a);
+        }
         return answer;
+
     }
 }
